1035e200619ddd22eabb6d180fb6d98b833f2018
toripi0380 <egasirbarino-hipdrop@ezweb.ne.jp>
2022-10-24 10:19:18 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
 
   private
-
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
