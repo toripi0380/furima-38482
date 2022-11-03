d3ba442a934cafa5d61b0bcfd27416b4ae694334
toripi0380 <egasirbarino-hipdrop@ezweb.ne.jp>
2022-11-03 21:51:01 +0900

    商品詳細表示機能

--- db/migrate/20221031013229_create_items.rb
+++ db/migrate/20221031013229_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :product, null: false
       t.text :description, null: false
       t.integer :category_id, null: false
-      t.integer :product_condition_id, null: false
+      t.integer :condition_id, null: false
       t.integer :shipping_charges_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :number_of_days_id, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_033408) do
+ActiveRecord::Schema.define(version: 2022_10_31_014935) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,21 +33,11 @@ ActiveRecord::Schema.define(version: 2022_10_31_033408) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "exhibits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "category_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "shipping_charges_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "number_of_days_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "product", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
-    t.integer "product_condition_id", null: false
+    t.integer "condition_id", null: false
     t.integer "shipping_charges_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "number_of_days_id", null: false
